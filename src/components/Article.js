18a43ea0ed6cb4a18e3b5a1d0ffe306cc95ea941
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-05 16:48:38 +0300

    add accordion/add reverse dataflow

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { findDOMNode } from "react-dom";
 import PropTypes from "prop-types";
 import CommentList from "./CommentList";
-import toggleOpen from "../decorators/toggleOpen";
 
 class Article extends Component {
   static propTypes = {
@@ -13,35 +12,13 @@ class Article extends Component {
     }).isRequired
   };
 
-  constructor(props) {
-    super(props);
-
-    this.containerRef = React.createRef();
-    this.commentsRef = React.createRef();
-  }
-
-  componentDidMount() {
-    console.log(this.containerRef.current);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.commentsRef.current); // компонент
-    console.log(findDOMNode(this.commentsRef.current)); // его реальная DOM-нода
-  }
-
-  // с коллбэком
-  // commentsRef = (node) => {
-  //   console.log(node);
-  //   console.log(findDOMNode(node));
-  // };
-
   getBody() {
     const { article, isOpen } = this.props;
     if (!isOpen) return null;
     return (
       <section>
         {article.text}
-        <CommentList ref={this.commentsRef} comments={article.comments} />
+        <CommentList comments={article.comments} />
       </section>
     );
   }
@@ -49,7 +26,7 @@ class Article extends Component {
   render() {
     const { article, isOpen, toggleOpen } = this.props;
     return (
-      <div ref={this.containerRef}>
+      <div>
         <h3>{article.title}</h3>
         <button onClick={toggleOpen}>{isOpen ? "close" : "open"}</button>
         {this.getBody()}
@@ -58,4 +35,4 @@ class Article extends Component {
   }
 }
 
-export default toggleOpen(Article);
+export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Article from "./Article";
 
-const ArticleList = ({ articles }) => {
-  const articleElements = articles.map((article) => (
-    <li key={article.id}>
-      <Article article={article} />
-    </li>
-  ));
+class ArticleList extends Component {
+  state = {
+    openArticleId: null
+  };
 
-  return <ul>{articleElements}</ul>;
-};
+  toggleOpenArticle = (id) => (event) => {
+    this.setState({ openArticleId: id });
+  };
+
+  render() {
+    const { articles } = this.props;
+    const { openArticleId } = this.state;
+    const articleElements = articles.map((article) => (
+      <li key={article.id}>
+        <Article
+          article={article}
+          isOpen={article.id === openArticleId}
+          toggleOpen={this.toggleOpenArticle(article.id)}
+        />
+      </li>
+    ));
+
+    return <ul>{articleElements}</ul>;
+  }
+}
 
 export default ArticleList;
