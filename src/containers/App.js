afb6e2fee623d794c286ba9b22ffa05027ba97b9
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-06 00:14:59 +0300

    add react-select

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { hot } from "react-hot-loader";
+import Select from "react-select";
 import ArticleList from "../components/ArticleList";
 import UserForm from "../components/UserForm";
 
@@ -9,11 +10,28 @@ class App extends Component {
     articles: PropTypes.array
   };
 
+  state = {
+    selectedOption: null
+  };
+
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log("Option selected: ", selectedOption);
+  };
+
   render() {
     const { articles } = this.props;
+    const { selectedOption } = this.state;
+
+    const options = articles.map((article) => ({
+      label: article.title,
+      value: article.id
+    }));
+
     return (
       <div>
         <UserForm />
+        <Select value={selectedOption} onChange={this.handleChange} options={options} />
         <ArticleList articles={articles} />
       </div>
     );
