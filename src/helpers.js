5b858406a4db0a99f6e3bdc1778df978aca94b33
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-09 13:52:16 +0300

    improve defaultArticledState immutable

--- src/helpers.js
+++ src/helpers.js
@@ -2,13 +2,20 @@ const { Map } = require("immutable");
 
 // export const arrToMap = (arr) => {
 //   return arr.reduce((acc, item) => {
-//     acc.set(item.id, item);
+//     acc.set(item.id,  new Map(item));
 //     return acc;
 //   }, new Map({}));
 // };
 
 // в одну строку es6
-export const arrToMap = (arr) => arr.reduce((acc, item) => acc.set(item.id, item), new Map({}));
+// каждую статью тоже заворачиваем в Map
+// export const arrToMap = (arr) =>
+//   arr.reduce((acc, item) => acc.set(item.id, new Map(item)), new Map({}));
+
+// когда мы завернули каждую статью тоже в map, то чтобы получить данные из статьи (id, title), нужно переписать всё наше приложение на get методы, т.к. теперь это Map структура и она отдаёт данные по ключ-значение, тоесть сейчас мы используем грубо говоря вот так store.getState().articles.get("56c782f18990ecf954f6e027").title, а нужно будет вот так store.getState().articles.get("56c782f18990ecf954f6e027").get("title"), чтобы этого не делать, в immutable есть такая структура как Record, она аналогична Map(тоже ключ-значение), но при инициализации которой мы можем описать наши поля(нашу схему/модель данных) и потом к ним обращаться как раньше напрямую, не вызывая функцию get
+
+export const arrToMap = (arr, DataRecord = Map) =>
+  arr.reduce((acc, item) => acc.set(item.id, new DataRecord(item)), new Map({}));
 
 export const mapToArr = (obj) => {
   return obj.valueSeq().toArray();
--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,6 +1,13 @@
-const { Map } = require("immutable");
+const { Map, Record } = require("immutable");
 import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES } from "../constants";
 import { arrToMap } from "../helpers";
+
+const ArticleRecord = Record({
+  id: undefined,
+  title: "",
+  text: undefined,
+  comments: []
+});
 
 const defaultArticledState = new Map({});
 
@@ -9,20 +16,19 @@ export default (articlesState = defaultArticledState, action) => {
 
   switch (type) {
     case LOAD_ALL_ARTICLES:
-      return arrToMap(response);
+      return arrToMap(response, ArticleRecord);
 
     case DELETE_ARTICLE:
       return articlesState.delete(payload.id);
 
     case ADD_COMMENT:
-      const article = articlesState[payload.articleId];
-      return {
-        ...articlesState,
-        [payload.articleId]: {
-          ...article,
-          comments: (article.comments || []).concat(randomId)
-        }
-      };
+      // такой вариант подходит только для верхнего уровня вложенности, так обновится поле comments: randomId, а нам нужно comments: [id1, id2, ..., randomId]
+      // return articleState.update(payload.articleId, (article) => article.set("comments", randomId));
+
+      // [где обновить? в payload.articleId, что обновить? поле "comments"], дальше получаем это поле (comments) => обновляем
+      return articleState.updateIn([payload.articleId, "comments"], (comments) =>
+        comments.concat(randomId)
+      );
   }
 
   return articlesState;
