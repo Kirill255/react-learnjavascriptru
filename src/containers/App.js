5b7fb472bfa0e365b8a446b7f76257769534c74e
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-10 00:33:16 +0300

    add base react-router-dom

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { hot } from "react-hot-loader";
 
 import ArticleList from "../components/ArticleList";
@@ -10,12 +11,34 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Counter />
-        <UserForm />
-        <Filters />
-        <ArticleList />
-      </div>
+      <Router>
+        <div className="app">
+          <div>
+            <h2>Main menu</h2>
+            <div>
+              <Link to="/">Home</Link>
+            </div>
+            <div>
+              <Link to="/counter">Counter</Link>
+            </div>
+            <div>
+              <Link to="/filters">Filters</Link>
+            </div>
+            <div>
+              <Link to="/articles">ArticleList</Link>
+            </div>
+          </div>
+          <UserForm />
+          <Route path="/counter" component={Counter} />
+          <Route path="/filters" component={Filters} />
+          <Route path="/articles" component={ArticleList} />
+          {/*
+          <Counter />
+          <Filters />
+          <ArticleList />
+          */}
+        </div>
+      </Router>
     );
   }
 }
